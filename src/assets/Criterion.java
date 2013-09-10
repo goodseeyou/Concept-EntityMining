6a1aeabdfefaa539905b6348ce19c6de74c8458b
paul <good_see_you@hotmail.com>
2013-09-11 07:50:36 +0800

    update

--- src/assets/Criterion.java
+++ src/assets/Criterion.java
@@ -4,16 +4,42 @@ import java.util.HashSet;
 
 public class Criterion extends ConceptFamily{
 	final String type = "criterion";
-	HashSet<RealConcept> realConcepts = new HashSet<RealConcept>();
+	boolean isRought;
+	boolean isInitialized = false;
+	HashSet<RealConcept> realConcepts;
+	
+	public Criterion(){
+		this.isRought = true;
+	}
+	public Criterion(HashSet<RealConcept> concepts){
+		this.realConcepts = concepts;
+		this.isRought = false;
+		this.isInitialized = true;
+	}
 	@Override
 	public String getType() {
 		return this.type;
 	}
 
+	public boolean addRealConcept(RealConcept concept){
+		if(this.realConcepts.add(concept))
+			return true;
+		else
+			return false;
+	}
 	@Override
 	public boolean add(Object object) {
-		// TODO Auto-generated method stub
-		return false;
+		if(object.getClass() != RealConcept.class)
+			return false;
+		else{
+			if(! this.isInitialized){
+				this.realConcepts = new HashSet<RealConcept>();
+			}
+			if(this.addRealConcept((RealConcept)object))
+				return true;
+			else
+				return false;
+		}
 	}
 
 }
